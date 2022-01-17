078e0bfaf1df7ee49698b1e96dbd8a49a593daf0
Hoai Sang <hoai.sang050@gmail.com>
2022-01-18 05:22:16 +0700

    newcommit

---
+++ src/components/Product/Component/Product.jsx
@@ -0,0 +1,64 @@
+import {Box, makeStyles, Typography} from '@material-ui/core';
+import {STATIC_HOST, THUMBNAIL_PLACEHOLDER} from 'Constant/common';
+import React from 'react';
+import {Rating} from '@material-ui/lab';
+import StarRoundedIcon from '@material-ui/icons/StarRounded';
+
+const useStyles = makeStyles((theme) => ({
+  price: {
+    fontWeight: '600',
+    fontSize: '1rem',
+  },
+  ratingStar: {
+    margin: theme.spacing(1, 0),
+  },
+}));
+
+function Product({product = {}}) {
+  const thumbnail = product.thumbnail?.url
+    ? `${STATIC_HOST}${product.thumbnail.url}`
+    : THUMBNAIL_PLACEHOLDER;
+
+  const pricePer =
+    Number.parseInt(product.promotionPercent) > 0
+      ? `- ${Number.parseInt(product.promotionPercent)}%`
+      : '';
+
+  const name = product.name
+    .toLowerCase()
+    .split(' ')
+    .map((x) => {
+      return x[0].toUpperCase() + x.substring(1) + ' ';
+    });
+
+  const price = `${new Intl.NumberFormat('vi-VN').format(
+    Number.parseInt(product.salePrice)
+  )} VND`;
+  const classes = useStyles();
+
+  const randomRating = () => {
+    return Math.floor(Math.random() * 5) + 1;
+  };
+
+  return (
+    <Box padding={1.2}>
+      <Box padding={1}>
+        <img src={thumbnail} />
+      </Box>
+
+      <Typography variant="body2">{name}</Typography>
+      <Rating
+        value={randomRating()}
+        size="small"
+        className={classes.ratingStar}
+        icon={<StarRoundedIcon fontSize="inherit" />}
+        readOnly={true}
+      />
+      <Typography variant="body2">
+        <span className={classes.price}>{`${price}`}</span> {pricePer}
+      </Typography>
+    </Box>
+  );
+}
+
+export default Product;
