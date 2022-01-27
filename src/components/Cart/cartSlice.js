09ae77da373f134d7e2955de5d083f48cdd67d8f
Hoai Sang <hoai.sang050@gmail.com>
2022-01-27 09:00:33 +0700

    hehehehe

---
+++ src/components/Cart/cartSlice.js
@@ -0,0 +1,42 @@
+const {createSlice} = require('@reduxjs/toolkit');
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState: {
+    showMiniCart: false,
+    cartItems: [],
+  },
+  reducers: {
+    showMiniCart(state) {
+      state.showMiniCart = true;
+    },
+    hideMiniCart(state) {
+      state.showMiniCart = false;
+    },
+    deleteItem(state, action) {
+      const id = action.payload;
+      state.cartItems = state.cartItems.filter((x) => x.id != id);
+    },
+    addNewItem(state, action) {
+      const newItem = action.payload;
+
+      const currentIndex = state.cartItems.findIndex(
+        (x) => x.id === newItem.id
+      );
+
+      if (currentIndex >= 0)
+        state.cartItems[currentIndex].quantity += newItem.quantity;
+      else state.cartItems.push(newItem);
+    },
+    setQuantity(state, action) {
+      const {id, quantity} = action.payload;
+      const index = state.cartItems.findIndex((x) => x.id === id);
+      state.cartItems[index].quantity = quantity;
+    },
+  },
+});
+
+const {actions, reducer} = cartSlice;
+export const {showMiniCart, hideMiniCart, addNewItem, deleteItem, setQuantity} =
+  actions;
+export default reducer;
